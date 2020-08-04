c9c9e91ea927dae31fb03f0e1b3f239242b596eb
belal <belal@wizely.in>
2020-08-04 20:22:07 +0530

    User Signup

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
 
 class AuthController extends Controller
 {
@@ -12,8 +13,26 @@ class AuthController extends Controller
         echo "Login Endpoint Requested";
     }
 
-    public function signup(){
-        echo "Signup Endpoint Requested";
+    public function signup(Request $request){
+
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+        
+        $user = new User([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            "message" => "User registered successfully"
+        ], 201);
+        
     }
 
     public function index(){
