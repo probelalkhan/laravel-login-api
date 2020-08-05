4bbbd4c58f7a56c93935391ddde294e7b01847db
belal <belal@wizely.in>
2020-08-05 15:03:00 +0530

    Login API Created

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,12 +5,35 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     
-    public function login(){
-        echo "Login Endpoint Requested";
+    public function login(Request $request){
+        
+        $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials)){
+            return response()->json([
+                'message'=> 'Invalid email or password'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('Access Token');
+
+        $user->access_token = $token->accessToken;
+
+        return response()->json([
+            "user"=>$user
+        ], 200);
     }
 
     public function signup(Request $request){
